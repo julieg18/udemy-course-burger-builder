61eda63592cab8c073ffd5796157887772c55c14
julieg18 <julieheartcrochet@gmail.com>
2019-07-23 08:12:19 -0500

    SECTION : 157-163 Calculating the Ingredient Sum Dynamically Adding the Build Control Component Outputting Multiple Build Controls Connecting State to Build Controls Removing Ingredients Safely Displaying and Updating the Burger Price Adding the Order Button

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,19 @@ import styles from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const Burger = (props) => {
-  const transformedIngredients = Object.keys(props.ingredients).map((igKey) => {
-    return [...Array(props.ingredients[igKey])].map((_, i) => {
-      return <BurgerIngredient key={igKey + i} ingredientType={igKey} />;
-    });
-  });
-
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map((igKey) => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} ingredientType={igKey} />;
+      });
+    })
+    .flat();
+  // .reduce((arr, el) => {
+  //   return arr.concat(el);
+  // }, []);
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>;
+  }
   return (
     <div className={styles.Burger}>
       <BurgerIngredient ingredientType="bread-top" />
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,24 +1,90 @@
 import React from 'react';
 import Burger from '../../components/Burger/Burger';
+import BurgerControls from '../../components/Burger/BuildControls/BuildControls';
+
+const INGREDIENT_PRICES = {
+  salad: 0.1,
+  cheese: 0.1,
+  meat: 0.5,
+  bacon: 0.2,
+};
 
 class BurgerBuilder extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       ingredients: {
-        salad: 2,
-        bacon: 2,
-        cheese: 2,
-        meat: 1,
+        salad: 0,
+        bacon: 0,
+        cheese: 0,
+        meat: 0,
       },
+      totalPrice: 1,
+      purchaseable: false,
     };
   }
 
+  updatePurchaseableState(ingredients) {
+    const sum = Object.values(ingredients).reduce((acc, val) => acc + val);
+    this.setState({
+      purchaseable: sum > 0,
+    });
+  }
+
+  addIngredientHandler = (ingredientType) => {
+    const oldCount = this.state.ingredients[ingredientType];
+    const updatedCount = oldCount + 1;
+    const updatedIngredients = {
+      ...this.state.ingredients,
+    };
+    updatedIngredients[ingredientType] = updatedCount;
+    const priceAddition = INGREDIENT_PRICES[ingredientType];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice + priceAddition;
+    this.setState({
+      ingredients: updatedIngredients,
+      totalPrice: newPrice,
+    });
+    this.updatePurchaseableState(updatedIngredients);
+  };
+
+  removeIngredientHandler = (ingredientType) => {
+    const oldCount = this.state.ingredients[ingredientType];
+    if (oldCount <= 0) {
+      return;
+    }
+    const updatedCount = oldCount - 1;
+    const updatedIngredients = {
+      ...this.state.ingredients,
+    };
+    updatedIngredients[ingredientType] = updatedCount;
+    const priceSubtraction = INGREDIENT_PRICES[ingredientType];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice - priceSubtraction;
+    this.setState({
+      ingredients: updatedIngredients,
+      totalPrice: newPrice,
+    });
+    this.updatePurchaseableState(updatedIngredients);
+  };
+
   render() {
+    const disabledInfo = {
+      ...this.state.ingredients,
+    };
+    for (let key in disabledInfo) {
+      disabledInfo[key] = disabledInfo[key] <= 0;
+    }
     return (
       <React.Fragment>
         <Burger ingredients={this.state.ingredients} />
-        <div>Build Controls</div>
+        <BurgerControls
+          ingredientRemoved={this.removeIngredientHandler}
+          ingredientAdded={this.addIngredientHandler}
+          disabled={disabledInfo}
+          price={this.state.totalPrice}
+          purchaseable={this.state.purchaseable}
+        />
       </React.Fragment>
     );
   }
