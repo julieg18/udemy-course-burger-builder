4a4d97d1b1f0a07d5d7635e23897e67ba844355e
julieg18 <julieheartcrochet@gmail.com>
2019-08-15 08:09:06 -0500

    Updates

---
+++ precache-manifest.696b1738c176f90e9b1a1bf04e83c88d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae3fb71948150b8b2056eef3c9005243",
+    "url": "/udemy-course-burger-builder/index.html"
+  },
+  {
+    "revision": "0fc7e18c0c174db2d860",
+    "url": "/udemy-course-burger-builder/static/css/main.6cf4ade0.chunk.css"
+  },
+  {
+    "revision": "43a6df2f9dc1cb6e682c",
+    "url": "/udemy-course-burger-builder/static/js/2.594fa91d.chunk.js"
+  },
+  {
+    "revision": "0fc7e18c0c174db2d860",
+    "url": "/udemy-course-burger-builder/static/js/main.f0820ebb.chunk.js"
+  },
+  {
+    "revision": "98d9102d4eed805e5747",
+    "url": "/udemy-course-burger-builder/static/js/runtime~main.cb0bd3f7.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/udemy-course-burger-builder/static/media/burger-logo.b8503d26.png"
+  }
+]);
